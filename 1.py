afcc4ca0b4dabc0c9c196c6a126a653b3995aad0
Шишкіна Каріна <ks6611788@gmail.com>
2024-10-10 18:24:42 +0300

    1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,3 @@
-caps = lambda text: [
-    i for i,
-    char in enumerate(text) 
-      if char.isupper()
-    ]
+caps = lambda text: [i for i, char in enumerate(text) if char.isupper()]
 s = input("Введіть текст: ")
 print(caps(s))
